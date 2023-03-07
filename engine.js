3277329b1b5b91ce01d61d35a6d28d4ccf9b10e9
James Stanley <james@incoherency.co.uk>
2023-03-07 00:00:51 +0000

    Initial commit

---
+++ engine.js
@@ -0,0 +1,130 @@
+
+var stop = false;
+function Engine() {
+    // parameters:
+    this.crankthrow = 15; // mm
+    this.portthrow = 12; // mm
+    this.deadspace = 5; // mm, between top of piston and top of cylinder
+    this.bore = 15; // mm
+    this.rodlength = 63; // mm
+    this.inletportdiameter = 2.5; // mm
+    this.exhaustportdiameter = 2.5; // mm
+    this.cylinderportdiameter = 2.0; // mm
+    this.inletportangle = -14.5; // degrees
+    this.exhaustportangle = 14.5; // degrees
+    this.pivotseparation = 67.5; // mm
+    this.flywheelmomentofinertia = 1.7e-4; // kg m^2
+    this.inletpressure = 20; // kPa
+    this.frictiontorque = 0.001; // Nm, opposing the flywheel rotation
+    this.airflowrate = 100; // TODO: units???
+
+    // state:
+    this.cylinderpressure = 0; // kPa
+    this.crankposition = 0; // degrees - TDC=0
+    this.rpm = 50; // rpm
+
+    // computed state:
+    this.cylinderangle = 0; // degrees
+    this.pistonheight = 0; // mm from top of cylinder
+    this.crankpinx = 0; // mm from crank centre
+    this.crankpiny = 0; // mm from crank centre
+
+    this.computeCylinderPosition();
+}
+
+Engine.prototype.step = function(dt) {
+    if (stop) return;
+    let pistonArea = Math.PI * (this.bore/2)*(this.bore/2);
+    let stroke = this.crankthrow*2;
+    let currentVolume = this.pistonheight * pistonArea;
+    let currentAirParticles = this.cylinderpressure * currentVolume;
+    let pressuredifference = this.inletpressure - this.cylinderpressure;
+
+    // compute the effective port locations
+    let inletPortX = sin(this.inletportangle * Math.PI/180) * this.portthrow;
+    let inletPortY = cos(this.inletportangle * Math.PI/180) * this.portthrow;
+    let exhaustPortX = sin(this.exhaustportangle * Math.PI/180) * this.portthrow;
+    let exhaustPortY = cos(this.exhaustportangle * Math.PI/180) * this.portthrow;
+    let cylinderPortX = sin(this.cylinderangle * Math.PI/180) * this.portthrow;
+    let cylinderPortY = cos(this.cylinderangle * Math.PI/180) * this.portthrow;
+
+    this.inletportx = inletPortX;
+    this.inletporty = inletPortY;
+    this.exhaustportx = exhaustPortX;
+    this.exhaustporty = exhaustPortY;
+    this.cylinderportx = cylinderPortX;
+    this.cylinderporty = cylinderPortY;
+
+    // compute port overlap areas
+    let inletPortArea = areaOfIntersection(inletPortX, inletPortY, this.inletportdiameter/2, cylinderPortX, cylinderPortY, this.cylinderportdiameter/2);
+    let exhaustPortArea = areaOfIntersection(exhaustPortX, exhaustPortY, this.exhaustportdiameter/2, cylinderPortX, cylinderPortY, this.cylinderportdiameter/2);
+
+    // 1. if inlet port is open, let some air in (proportional to pressure difference and port area)
+    let inletAirParticles = pressuredifference * inletPortArea * this.airflowrate;
+
+    // 2. if exhaust port is open, let some air out (proportional to pressure difference and port area)
+    let exhaustAirParticles = this.cylinderpressure * exhaustPortArea * this.airflowrate;
+
+    // 3. calculate updated cylinder pressure
+    this.cylinderpressure = (currentAirParticles + inletAirParticles - exhaustAirParticles) / currentVolume;
+
+    // 4. calculate torque from piston
+    pistonForce = 1000 * this.cylinderpressure * (pistonArea * 0.000001); // Newtons
+    pistonActingDistance = -Math.sin(this.cylinderangle * Math.PI/180) * this.pivotseparation;
+    crankTorque = pistonForce * (pistonActingDistance * 0.001); // Nm
+
+    // 5. calculate flywheel angular velocity with piston torque
+    let oldrpm = this.rpm;
+    this.rpm += crankTorque / this.flywheelmomentofinertia * dt;
+
+    // 6. apply friction torque
+    let friction_deltarpm = this.frictiontorque / this.flywheelmomentofinertia * dt;
+    if (Math.abs(friction_deltarpm) > Math.abs(this.rpm)) {
+        this.rpm = 0;
+    } else if (this.rpm > 0) {
+        this.rpm -= friction_deltarpm;
+    } else {
+        this.rpm += friction_deltarpm;
+    }
+
+    this.rpm -= this.rpm * 0.01*dt;
+
+    // 7. update crank position (first pass)
+    this.crankposition += (this.rpm * 360 / 60) * dt;
+    while (this.crankposition > 360) this.crankposition -= 360;
+
+    this.computeCylinderPosition();
+};
+
+Engine.prototype.computeCylinderPosition = function() {
+    // 1. find position of crank pin relative to crank centre
+    this.crankpinx = Math.sin(this.crankposition * PI/180) * this.crankthrow;
+    this.crankpiny = Math.cos(this.crankposition * PI/180) * this.crankthrow;
+
+    // 2. find angle from crank pin to cylinder pivot
+    let dx = this.crankpinx;
+    let dy = this.pivotseparation - this.crankpiny;
+    this.cylinderangle = Math.atan2(dy, dx) * 180/PI - 90;
+
+    // 3. find height of piston
+    let dist = Math.sqrt(dx*dx + dy*dy);
+    this.pistonheight = this.deadspace + this.crankthrow + dist - this.pivotseparation;
+}
+
+// https://math.stackexchange.com/a/290526
+function areaOfIntersection(x0, y0, r0, x1, y1, r1) {
+  var rr0 = r0*r0;
+  var rr1 = r1*r1;
+  var c = Math.sqrt((x1-x0)*(x1-x0) + (y1-y0)*(y1-y0));
+  if (c >= r0+r1) return 0; // no overlap: return 0 area
+  if (c < r0 || c < r1) {
+      // full overlap: return area of smallest circle
+      if (r0 < r1) return Math.PI*r0*r0;
+      else return Math.PI*r1*r1;
+  }
+  var phi = (Math.acos((rr0+(c*c)-rr1) / (2*r0*c)))*2;
+  var theta = (Math.acos((rr1+(c*c)-rr0) / (2*r1*c)))*2;
+  var area1 = 0.5*theta*rr1 - 0.5*rr1*Math.sin(theta);
+  var area2 = 0.5*phi*rr0 - 0.5*rr0*Math.sin(phi);
+  return area1 + area2;
+}
---
+++ main.js
@@ -0,0 +1,124 @@
+var canvas;
+var engine;
+
+var flywheeldiameter = 68; // mm
+
+var piston_height_px = 10;
+
+var canvasmargin = 20; // px
+var px_per_mm;
+
+function setup() {
+    canvas = createCanvas(400, 400);
+    canvas.parent('canvas');
+
+    engine = new Engine();
+
+    let totalheight_mm = flywheeldiameter/2 + engine.crankthrow + engine.rodlength + engine.deadspace; // mm
+    let availableheight_px = canvas.height - 2*canvasmargin;
+    px_per_mm = availableheight_px / totalheight_mm;
+}
+
+function draw() {
+    engine.step(deltaTime / 1000.0);
+
+    background(220);
+
+    drawFlywheel();
+    drawCylinder();
+    drawPiston();
+    drawPorts();
+    drawPivot();
+
+    document.getElementById('rpm').innerText = Math.round(engine.rpm);
+}
+
+function drawFlywheel() {
+    let diameter = flywheeldiameter * px_per_mm;
+
+    let centre_px = canvas.height - canvasmargin - diameter/2;
+
+    push();
+
+    translate(canvas.width/2, centre_px);
+
+    circle(0, 0, diameter); // flywheel
+
+    pop();
+}
+
+function drawCylinder() {
+    push();
+
+    // height of pivot from top of cylinder
+    let pivot_height_mm = engine.deadspace + engine.crankthrow + engine.rodlength - engine.pivotseparation;
+    let pivot_height_px = canvasmargin + pivot_height_mm * px_per_mm;
+
+    // cylinder dimensions
+    let cylinder_height_mm = engine.deadspace + 2*engine.crankthrow;
+    let cylinder_width_mm = engine.bore;
+    let cylinder_height_px = cylinder_height_mm * px_per_mm + piston_height_px
+    let cylinder_width_px = cylinder_width_mm * px_per_mm;
+
+    translate(canvas.width/2, pivot_height_px);
+    rotate(engine.cylinderangle * PI/180);
+
+    rect(-cylinder_width_px/2, -pivot_height_mm*px_per_mm, cylinder_width_px, cylinder_height_px); // main cylinder
+    circle(0, -engine.portthrow * px_per_mm, engine.cylinderportdiameter * px_per_mm); // cylinder port
+
+    noStroke();
+    let atmospheric_colour = color(0,255,255);
+    let inlet_colour = color(255,0,0);
+    let gas_colour = lerpColor(atmospheric_colour, inlet_colour, engine.cylinderpressure/engine.inletpressure);
+    gas_colour.setAlpha(127);
+    fill(gas_colour);
+    rect(-cylinder_width_px/2, -pivot_height_mm*px_per_mm, cylinder_width_px, engine.pistonheight * px_per_mm); // cylinder gases
+
+    pop();
+}
+
+function drawPiston() {
+    let diameter = flywheeldiameter * px_per_mm;
+    let centre_px = canvas.height - canvasmargin - diameter/2;
+
+    push();
+
+    translate(canvas.width/2, centre_px);
+    rotate(engine.crankposition * PI/180);
+
+    rect(-5, 0, 10, -engine.crankthrow * px_per_mm); // crank
+    circle(0, 0, 10);
+
+    translate(0, -engine.crankthrow * px_per_mm);
+    rotate((-engine.crankposition + engine.cylinderangle) * PI/180);
+    rect(-5, 0, 10, -engine.rodlength * px_per_mm); // rod
+    circle(0, 0, 10);
+    translate(0, -engine.rodlength * px_per_mm);
+    let bore_mm = engine.bore * px_per_mm;
+    rect(-bore_mm/2, 0, bore_mm, piston_height_px); // piston
+
+    pop();
+}
+
+function drawPorts() {
+    let diameter = flywheeldiameter * px_per_mm;
+    let centre_px = canvas.height - canvasmargin - diameter/2 - engine.pivotseparation*px_per_mm;
+
+    push();
+
+    translate(canvas.width/2, centre_px);
+    circle(engine.inletportx*px_per_mm, -engine.inletporty*px_per_mm, engine.inletportdiameter*px_per_mm);
+    circle(engine.exhaustportx*px_per_mm, -engine.exhaustporty*px_per_mm, engine.exhaustportdiameter*px_per_mm);
+    circle(engine.cylinderportx*px_per_mm, -engine.cylinderporty*px_per_mm, engine.cylinderportdiameter*px_per_mm);
+
+    pop();
+}
+
+function drawPivot() {
+    let diameter = flywheeldiameter * px_per_mm;
+
+    let pivot_centre_px = canvas.height - canvasmargin - diameter/2 - engine.pivotseparation*px_per_mm;
+
+    circle(canvas.width/2, pivot_centre_px, 2);
+}
+
